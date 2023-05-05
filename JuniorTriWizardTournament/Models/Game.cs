6140cfe0c4e912ae14620beac7b1059fa39664f4
Jenny Kitchen <jennykayekitchen@gmail.com>
2023-05-05 14:40:06 -0500

    total points for the user

---
+++ JuniorTriWizardTournament/Models/Game.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JuniorTriWizardTournament.Models
+{
+    public class Game
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public int TotalPoints { get; set; }
+    }
+}
--- JuniorTriWizardTournament/Startup.cs
+++ JuniorTriWizardTournament/Startup.cs
@@ -34,7 +34,8 @@ namespace JuniorTriWizardTournament
             services.AddTransient<ICategoryRepository, CategoryRepository>();
             services.AddTransient<ISubjectRepository, SubjectRepository>();
             services.AddTransient<IWordRepository, WordRepository>();
-            
+            services.AddTransient<IGameRepository, GameRepository>();
+
             var firebaseProjectId = Configuration.GetValue<string>("FirebaseProjectId");
             var googleTokenUrl = $"https://securetoken.google.com/{firebaseProjectId}";
             services
